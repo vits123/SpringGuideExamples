c41e83888d7c905cf457dcc7635d4ea634744b7c
Vedant <vedantrahane01@gmail.com>
2020-03-15 21:27:06 -0500

    Stomp based message processing using Spring Boot

---
+++ websocket-ws-app/src/main/java/com/websocket/ws/app/messagingstompwebsocket/Greeting.java
@@ -0,0 +1,22 @@
+package com.websocket.ws.app.messagingstompwebsocket;
+
+public class Greeting {
+  
+  private String content;
+
+  public Greeting() {
+    super();
+  }
+
+  public Greeting(String content) {
+    super();
+    this.content = content;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  
+
+}
