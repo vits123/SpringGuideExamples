32067325c71678e04a62ea0ea5aed65dc57951c2
Vedant <vedantrahane01@gmail.com>
2020-03-15 20:15:42 -0500

    Updated Pom.xml

---
+++ websocket-ws-app/src/test/java/com/websocket/ws/app/WebsocketWsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.ws.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketWsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
